37a17b65c91d8fe878937b50d0777f10e449df73
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-08-21 16:21:53 -0300

    Initial commit | Console.log
    Commit inicial, e aula inicial de console log adicionada.

---
+++ aula_3/index.js
@@ -0,0 +1,5 @@
+console.log("Marcelo 'Anselmo'") // String = Texto
+console.log('Marcelo "Anselmo"') //Number = Número
+console.log(`Marcelo "Anselmo"`)
+
+console.log(35, 15.85, 'Marcelo Anselmo De Novo')
