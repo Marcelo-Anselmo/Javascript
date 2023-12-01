35aa7f5baa52a8e684c24ef21665ef4381e4e085
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-12-01 19:32:48 -0300

    aula de arrays updated!

--- Aulas_UDM/listas-arrays/aula.js
+++ Aulas_UDM/listas-arrays/aula.js
@@ -1,6 +1,15 @@
+const notas = [];
 
+notas.push(5);
+notas.push(10);
+notas.push(10);
 
-const alunos = ["João", "Vitor", "Marina"];
+let soma = 0;
 
+for(let i = 0; i < notas.length; i++){
+    const nota = notas[i];
+    soma = soma + nota;
+}
 
-console.log(alunos)
+const media = soma / notas.length
+console.log(media);
